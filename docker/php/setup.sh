94d4773f1f408686da6c941521cc762ef05375b9
Kailash Sharma <kailash_kds@yahoo.com>
2025-05-19 12:56:19 +0530

    Fix setup.sh

--- docker/php/setup.sh
+++ docker/php/setup.sh
@@ -2,7 +2,6 @@
 echo "185.199.108.133 raw.githubusercontent.com" >> /etc/hosts
 cd /var/www/html
 composer install
-php bin/console make:migration
 php bin/console doctrine:migrations:migrate
 php bin/console doctrine:fixtures:load
 php bin/console lexik:jwt:generate-keypair --skip-if-exists
